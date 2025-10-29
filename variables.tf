ae889f6a91e821b22b1dc5aca71d3504c36293b3
harish44412 <harisharun4442@gmail.com>
2025-10-29 17:33:38 +0530

    added commands in all files

--- variables.tf
+++ variables.tf
@@ -1,3 +1,8 @@
-variable "region" {}
+variable "region" {
+  description = "AWS region"
+  default     = "us-east-1"
+}
 variable "ami" {}
-variable "instance_type" {}
+variable "instance_type" {
+  default = "t2.micro"
+}
