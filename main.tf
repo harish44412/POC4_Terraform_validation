e6ed0946716d7311a5d8b0805d56872b7d55d3c5
harish44412 <harisharun4442@gmail.com>
2025-10-29 17:18:29 +0530

    Pushing local to repo

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = var.region
+}
+ 
+resource "aws_instance" "example" {
+  ami           = var.ami
+  instance_type = var.instance_type
+}
+ 
+output "instance_id" {
+  value = aws_instance.example.id
+}
